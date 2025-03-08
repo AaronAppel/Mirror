b54e5d709173e7c8e81d808d6ec69b5370df9541
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2025-03-08 12:11:14 -0500

    [CleanUp] Handling all clean up in root bat file
    Deleted redundant clean up bat files nested in each project directory after seeing the 2 star "**.user" approach to handle sub directories

--- Cleanup.bat
+++ Cleanup.bat
@@ -1,15 +1,9 @@
 del *.sln
+del **.vcxproj
+del **.user
 
 del /Q /F /S /AH bin
 rmdir /Q /S bin
 
 del /Q /F /S /AH .vs
 rmdir /Q /S ".vs"
-
-pushd Mirror
-call Cleanup.bat
-popd
-
-pushd Projects
-call Cleanup.bat
-popd
